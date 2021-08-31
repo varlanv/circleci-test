85ecd6933457eeead25a5107b9f58d84affbbb41
Vlad Varlan <Vladislav.Varlan-external@sema4genomics.com>
2021-08-31 13:55:09 +0300

    1

--- src/test/groovy/ua/vvarlan/circlecitest/CircleciTestApplicationTests.groovy
+++ src/test/groovy/ua/vvarlan/circlecitest/CircleciTestApplicationTests.groovy
@@ -1,20 +1,27 @@
 package ua.vvarlan.circlecitest
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.Rollback
+import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
 
+@Transactional
 @SpringBootTest
+@AutoConfigureTestEntityManager
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CircleciTestApplicationTests extends Specification {
 
+    @Autowired
     TestEntityManager testEntityManager
 
-    @Rollback(false)
     def 'context loads'() {
         when:
         testEntityManager.flush()
         1 == 1
+
         then:
         1 == 1
     }
